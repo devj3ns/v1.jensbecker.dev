bf2a7ce334fb91de88a0bdf1c7c88a42c0461372
Jens Becker <jensbecker01@gmx.de>
2021-04-27 22:49:19 +0200

    Update Beamer to lastest version

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:beamer/beamer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 
@@ -12,7 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await EasyLocalization.ensureInitialized();
-  setPathUrlStrategy();
+  Beamer.setPathUrlStrategy();
 
   runApp(
     EasyLocalization(
