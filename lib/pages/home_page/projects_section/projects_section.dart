118dfad8a7baa57913398795b864481e182c059b
Jens Becker <info@jensbecker.dev>
2021-08-03 18:57:03 +0200

    Add open source projects info to project section
    closes #9
    
    c

--- lib/pages/home_page/projects_section/projects_section.dart
+++ lib/pages/home_page/projects_section/projects_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../shared/shared_widgets.dart';
 import 'data/projects.dart';
@@ -14,13 +16,33 @@ class ProjectsSection extends StatelessWidget {
       title: tr('projects_section_title'),
       subtitle: tr('projects_section_subtitle'),
       topMarginMultiplier: 2,
-      child: ShadowBox(
-        child: Wrap(
-          spacing: 12,
-          runSpacing: 12,
-          children:
-              projects.map((project) => ProjectDetailsCard(project)).toList(),
-        ),
+      child: Column(
+        children: [
+          ShadowBox(
+            child: Wrap(
+              spacing: 12,
+              runSpacing: 12,
+              children: projects
+                  .map((project) => ProjectDetailsCard(project))
+                  .toList(),
+            ),
+          ),
+          const SizedBox(height: 35),
+           SelectableText(
+            tr('projects_open_source_info'),
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w200,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 5),
+          Button(
+            text: 'GitHub',
+            iconData: FontAwesomeIcons.github,
+            onPressed: () => launch('https://github.com/devj3ns'),
+          ),
+        ],
       ),
     );
   }
